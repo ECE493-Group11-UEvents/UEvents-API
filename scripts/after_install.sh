0abe20eb35228d9fb618f2c0ae9c86afc34de4fb
syjiao@ualberta.ca <syjiao@ualberta.ca>
2023-03-09 19:57:17 -0700

    fixing application start and finish scripts

--- scripts/after_install.sh
+++ scripts/after_install.sh
@@ -3,7 +3,7 @@ folder_name="UEvents-API"
 
 echo 'run after_install.sh: ' >> /home/ec2-user/"$folder_name"/deploy.log
 
-echo 'cd /home/ec2-user/nodejs-server-cicd' >> /home/ec2-user/"$folder_name"/deploy.log
+echo 'cd /home/ec2-user/UEvents-API' >> /home/ec2-user/"$folder_name"/deploy.log
 cd /home/ec2-user/"$folder_name" >> /home/ec2-user/"$folder_name"/deploy.log
 
 echo 'npm install' >> /home/ec2-user/"$folder_name"/deploy.log 
--- scripts/application_start.sh
+++ scripts/application_start.sh
@@ -3,5 +3,5 @@ folder_name="UEvents-API"
 
 echo 'run application_start.sh: ' >> /home/ec2-user/"$folder_name"/deploy.log
 
-echo 'pm2 restart nodejs-express-app' >> /home/ec2-user/"$folder_name"/deploy.log
-pm2 restart nodejs-express-app >> /home/ec2-user/"$folder_name"/deploy.log
+echo 'pm2 restart UEvents-API' >> /home/ec2-user/"$folder_name"/deploy.log
+pm2 restart "$folder_name" >> /home/ec2-user/"$folder_name"/deploy.log
