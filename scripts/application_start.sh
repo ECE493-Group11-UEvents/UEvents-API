c2ee9f8492df81b9bb13a38a8ca5276dce546e71
syjiao@ualberta.ca <syjiao@ualberta.ca>
2023-03-30 17:12:34 -0600

    finally fixed env vars for app

--- scripts/application_start.sh
+++ scripts/application_start.sh
@@ -4,4 +4,4 @@ folder_name="UEvents-API"
 echo 'run application_start.sh: ' >> /home/ec2-user/"$folder_name"/deploy.log
 
 echo 'pm2 restart UEvents' >> /home/ec2-user/"$folder_name"/deploy.log
-pm2 restart UEvents >> /home/ec2-user/"$folder_name"/deploy.log
+sudo DB_ACCESS_KEY=$DB_ACCESS_KEY DB_SECRET_ACCESS_KEY=$DB_SECRET_ACCESS_KEY REGION=$REGION pm2 restart UEvents --update-env >> /home/ec2-user/"$folder_name"/deploy.log
