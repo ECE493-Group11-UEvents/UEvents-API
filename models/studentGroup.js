794af7a4becdc4e052707800e2c2a75c6b8483d0
Paige Lekach <paige.lekach@gmail.com>
2023-04-03 00:21:20 -0600

    group profile, edit, follow, unfollow

--- models/studentGroup.js
+++ models/studentGroup.js
@@ -1,5 +1,8 @@
 const AWS = require('aws-sdk');
 const MemberGroupModel = require('./memberGroup');
+const uuid = require('uuid');
+
+const DEFAULT_EVENT_PICTURE = `https://${process.env.BUCKET_NAME}.s3.${process.env.REGION}.amazonaws.com/default_event_photo.png`;
 
 AWS.config.update({
   region: process.env.REGION,
@@ -8,6 +11,7 @@ AWS.config.update({
 });
 
 const client = new AWS.DynamoDB();
+const s3 = new AWS.S3();
 
 const tableName = 'StudentGroups';
 
@@ -51,6 +55,178 @@ class StudentGroupModel {
             console.error(err);
             return null;
         }
+    }
+
+    static async editStudentGroup(group_id, group_name, description, photo, link_to_photo = null){
+        if(await this.groupExists(group_id)){
+            let photo_url = "";
+
+            if(photo){
+                const params = {
+                    Bucket: process.env.BUCKET_NAME,
+                    Key: uuid.v4() + photo.originalname,
+                    Body: photo.buffer,
+                    ContentType: photo.mimetype,
+                    ACL: 'public-read'
+                };
+
+                await s3.putObject(params).promise();
+                photo_url = `https://${process.env.BUCKET_NAME}.s3.${process.env.REGION}.amazonaws.com/${params.Key}`;
+                
+            }
+            else if (link_to_photo.startsWith(`https://${process.env.BUCKET_NAME}.s3.${process.env.REGION}.amazonaws.com/`)) {
+                photo_url = link_to_photo;
+            }
+
+            const item = {
+                "group_name": {"S": group_name},
+                "description": {"S": description},
+                "group_photo": {"S": photo_url || DEFAULT_EVENT_PICTURE},
+            };
+
+            var params = {
+                TableName: tableName,
+                Key: {
+                    group_id: { N: group_id }
+                },
+                UpdateExpression: 'set group_name = :value1, description = :value2, group_photo = :value3',
+                ExpressionAttributeValues: {
+                    ':value1': { S: group_name },
+                    ':value2': { S: description },
+                    ':value3': { S: photo_url || DEFAULT_EVENT_PICTURE}
+                },
+                ReturnValues: 'UPDATED_NEW'
+            };
+
+            try{
+                client.updateItem(params).promise()
+                    .then((data) => {
+                        console.log(data);
+                        return item;
+                    })
+                    .catch((err) => {
+                        return err
+                    });
+            } catch(error){
+                console.error(error);
+                throw error;
+            }
+        } else{
+            return "Group does not exist";
+        }
+    }
+
+    static async groupExists(group_id){
+        const params = {
+            TableName: tableName,
+            Key: {
+                group_id: { N: group_id }
+            }
+        };
+
+        try {
+            const result = await client.getItem(params).promise();
+
+            if( result.Item == null){
+                return false;
+            }
+
+            return true;
+        } catch(err){
+            console.error(err);
+            return null;
+        }
+    }
+
+    static async getHostedEvents(group_id){
+        const params = {
+            TableName: "Events",
+        };
+
+        try {
+            let result = await client.scan(params).promise();
+            result = result.Items.filter(event => {
+                return event.event_coordinator_group_id.N === group_id
+            })
+            return result;
+        } catch (err) {
+            console.error(err);
+            return null;
+        }
+    };
+
+    static async getFollowers(group_id){
+        const params = {
+            TableName: "FollowGroup"
+        };
+
+        try {
+            let result = await client.scan(params).promise();
+            result = result.Items.filter(user => {
+                return user.group_id.N === group_id
+            })
+            return result;
+        } catch (err) {
+            console.error(err);
+            return null;
+        }
+
+    }
+
+    static async isFollowing(group_id, follower){
+        const params = {
+            TableName: "FollowGroup"
+        };
+
+        try {
+            let result = await client.scan(params).promise();
+            result = result.Items.filter(user => {
+                return user.group_id.N === group_id & user.email.S === follower
+            })
+            return result.length > 0;
+        } catch (err) {
+            console.error(err);
+            return null;
+        }
+    }
+
+    static async follow(group_id, follower){
+        try{
+            var params = {
+                TableName: 'FollowGroup',
+                Item: {
+                    email: { S: follower },
+                    group_id: { N: group_id },
+                },
+            };
+
+            console.log(params)
+            await client.putItem(params).promise();
+
+            return { message: `Successfully followed ${group_id}` };
+        }
+        catch(err){
+            console.error(err);
+            return null;
+        }
+    }
+
+    static async unfollow(group_id, follower){
+        try{
+            var params = {
+                TableName: 'FollowGroup',
+                Key: {
+                  group_id: { N: group_id },
+                  email: { S: follower }
+                }
+              };
+              await client.deleteItem(params).promise();
+              return { message: `Successfully unfollowed ${group_id}` };
+        }
+        catch(err){
+            console.error(err);
+            return null;
+        } 
     }
 }
 
--- routes/studentGroups.js
+++ routes/studentGroups.js
@@ -1,6 +1,9 @@
 const express = require('express');
 const router = express.Router();
 const StudentGroupModel = require('../models/studentGroup');
+const multer = require('multer');
+
+const upload = multer();
 
 // Handle password change requests
 router.get('/user/:email', async (req, res) => {
@@ -17,13 +20,71 @@ router.get('/user/:email', async (req, res) => {
 router.get('/:id', async (req, res) => {
     const id = req.params.id;
 
-    const studentGroup = await StudentGroupModel.getStudentGroupById(id);
+    let studentGroup = await StudentGroupModel.getStudentGroupById(id);
     if (studentGroup) {
-        res.send(studentGroup.Item);
+        studentGroup = studentGroup.Item;
+
+        const events = await StudentGroupModel.getHostedEvents(id);
+        const followers = await StudentGroupModel.getFollowers(id);
+        // TODO: add the members of the group as well
+        // const members = await StudentGroupModel.getMembers(id);
+        studentGroup.events = events
+        studentGroup.followers = followers
+        res.send(studentGroup);
     } else {
         res.send('Failed to get student group');
     }
 });
 
+router.post('/edit/:group_id', upload.single('photo'), async (req, res) => {
+    const { group_id } = req.params;
+
+    const { group_name, description, photo_url} = req.body;
+    const photo = req.file;
+
+    try {
+        StudentGroupModel.editStudentGroup(group_id, group_name,description, photo, photo_url)
+        .then(result => {
+            res.send(result);
+        })
+        .catch(err => {
+            console.error(err);
+            res.status(500).send('Error editing group')
+        })
+    }
+    catch(err) {
+        console.error(err);
+        res.status(500).send('Error editing group');
+    }
+});
+
+router.post('/:group_id/follow', async (req, res) => {
+    const { group_id } = req.params;
+    const {follower_email} = req.body
+
+    const isFollowing = await StudentGroupModel.isFollowing(group_id, follower_email);
+
+    if(isFollowing){
+        res.status(400).json({ message: `${follower_email} is already following ${group_id}` });    
+    } else {
+        const followSuccess = await StudentGroupModel.follow(group_id, follower_email);
+        res.send(followSuccess);
+    }
+});
+
+router.delete('/:group_id/unfollow', async (req, res) => {
+    const { group_id } = req.params;
+    const {follower_email} = req.body
+
+    const isFollowing = await StudentGroupModel.isFollowing(group_id, follower_email);
+
+    if(isFollowing){
+        const unfollowSuccess = await StudentGroupModel.unfollow(group_id, follower_email);
+        res.send(unfollowSuccess);
+    } else {
+        res.status(400).json({ message: `${follower_email} is not following ${group_id}` });    
+        
+    }
+});
 
 module.exports = router;
