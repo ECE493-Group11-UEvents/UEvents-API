896085886ea1e6fa91aad3071a77fc5524318836
saji2000 <sajaddaneshmand79@gmail.com>
2023-04-03 22:40:22 -0600

    We can now view EC requests, reject or approve them

--- models/studentGroup.js
+++ models/studentGroup.js
@@ -1,5 +1,7 @@
 const AWS = require('aws-sdk');
 const MemberGroupModel = require('./memberGroup');
+const { v4: uuidv4 } = require('uuid');
+const e = require('express');
 
 AWS.config.update({
   region: process.env.REGION,
@@ -7,46 +9,190 @@ AWS.config.update({
   secretAccessKey: process.env.DB_SECRET_ACCESS_KEY,
 });
 
+const DEFAULT_PROFILE_PICTURE = `https://${process.env.BUCKET_NAME}.s3.${process.env.REGION}.amazonaws.com/default_user_photo.jpg`;
+
 const client = new AWS.DynamoDB();
 
 const tableName = 'StudentGroups';
 
 class StudentGroupModel {
 
-    static async requestStudentGroup(email, description, id){
-        var result;
+    static async requestIdExists(id){
         try{
-            // if(id && this.getStudentGroupById(id)){
+
+            var params = {
+                TableName: 'Requests',
+                IndexName: 'group_id-email-index',
+                KeyConditionExpression: 'group_id = :id',
+                ExpressionAttributeValues: {
+                    ':id': { N: id }
+                }
+            }
+
+            const result = await client.query(params).promise();
+            console.log(result);
+            if(result.Items.length === 0){
+                return false;
+            }
+            return true;
+        }
+        catch (err) {
+            console.error(err);
+            return null;
+        }
+    }
+
+    static async requestStudentGroup(email, description, id, group_name){
+
+        try{
+
+            if(id == -1){
+                
+                var random_id = Math.floor(Math.random() * -100000); 
+                console.log(random_id);
+                console.log(await this.requestIdExists(id));
+                // while(await this.requestIdExists(id)){
+                //     random_id = Math.floor(Math.random() * 100000); 
+                // }
+
                 var params = {
                     TableName: 'Requests',
                     Item: {
                     'email': { S: email },
-                    'group_id-role': { S: id+"-event_coordinator"},
+                    'group_id': { N: random_id.toString()},
                     'decision': { S: "pending"},
                     'description': { S: description },
-                    'id': { N: id }
+                    'group_name': {S: group_name}
                     }
                 };
-                result = await client.putItem(params).promise();
-            // }
-            // else{
-            //     var params = {
-            //         TableName: 'Requests',
-            //         Item: {
-            //         'email': { S: email },
-            //         'decision': {S: "pending"},
-            //         'description': { S: description },
-            //         'id': { N: null }
-            //         }
-            //     };
-            //     result = await client.putItem(params).promise();
-            // }
+            }
+            else{
+                var params = {
+                    TableName: 'Requests',
+                    Item: {
+                    'email': { S: email },
+                    'group_id': { N: id},
+                    'decision': { S: "pending"},
+                    'description': { S: description },
+                    'group_name': {S: group_name}
+                    }
+                };
+            }
+            
+            const result = await client.putItem(params).promise();
             return result;
         }
         catch (err) {
             console.error(err);
             return null;
         }
+    }
+
+    /**
+     * Retrieve all requests with a specific decision status from the 'Requests' table.
+     * @param {string} status - The decision status to filter requests by.
+     * @returns {Promise<object>} - A Promise that resolves to an object containing the results of the query.
+     */
+    static async viewRequests(status){
+        
+        var params = {
+            TableName: 'Requests',
+            IndexName: 'decision-index',
+            KeyConditionExpression: 'decision = :decision',
+            ExpressionAttributeValues: {
+                ':decision': { S: status }
+            }
+        }
+
+        const result = await client.query(params).promise();
+        return result;
+    }
+
+    /**
+     * Reject a request in the 'Requests' table for a specific email and group ID.
+     * @param {string} email - The email address of the requester.
+     * @param {number} group_id - The ID of the group the requester is requesting to join.
+     * @returns {Promise<boolean>} - A Promise that resolves to a boolean indicating whether the request was successfully rejected.
+     */
+    static async rejectRequest(email, group_id){
+        var params = {
+            TableName: 'Requests',
+            Key: {
+                email: { S: email },
+                group_id: { N: group_id },
+            },
+            UpdateExpression: 'set #decision = :decision',
+            ExpressionAttributeNames: {
+            '#decision': 'decision'
+            },
+            ExpressionAttributeValues: {
+                ':decision': { S: 'rejected' }
+            },
+            ReturnValues: 'ALL_NEW'
+        }
+
+        try {
+            const result = await client.updateItem(params).promise();
+            return result.Attributes !== undefined;
+        } catch (error) {
+            console.error('Error rejecting request:', error);
+            return false;
+        }
+    }
+
+    /**
+     * Accept a request in the 'Requests' table for a specific email and group ID, adding the requester as a member of the group.
+     * @param {string} email - The email address of the requester.
+     * @param {number} id - The ID of the group the requester is requesting to join.
+     * @param {string} name - The name of the group the requester is requesting to join.
+     * @returns {Promise<boolean>} - A Promise that resolves to a boolean indicating whether the request was successfully accepted and the requester added as a member of the group.
+     */
+    static async acceptRequest(email, id, name){
+
+        id = parseInt(id);
+
+        // creating a group if it doesn't exist'
+        if(id < 0){
+            var params = {
+                TableName: 'StudentGroups',
+                Item: {
+                  group_id: {N: Math.abs(id).toString()},
+                  group_name: {S: name},
+                  group_photo: {S: DEFAULT_PROFILE_PICTURE},
+                },
+              };
+            await client.putItem(params).promise();
+        }
+
+        // adding the coordinator
+        var params = {
+            TableName: 'MemberGroup',
+            Item: {
+                email: {S: email},
+                group_id: {N: Math.abs(id).toString()},
+            }
+        }
+        await client.putItem(params).promise();
+
+        // approving the request
+        params = {
+            TableName: 'Requests',
+            Key: {
+                email: { S: email },
+                group_id: { N: id.toString() },
+            },
+            UpdateExpression: 'set #decision = :decision',
+            ExpressionAttributeNames: {
+            '#decision': 'decision'
+            },
+            ExpressionAttributeValues: {
+                ':decision': { S: 'approved' }
+            },
+            ReturnValues: 'ALL_NEW'
+        }
+        await client.updateItem(params).promise();
+
+        return true;
     }
 
     /**
--- routes/studentGroups.js
+++ routes/studentGroups.js
@@ -26,11 +26,46 @@ router.get('/:id', async (req, res) => {
 });
 
 router.post('/request', async (req, res) => {
-    const {email, description, id} = req.body;
+    const {email, description, id, group_name} = req.body;
 
-    const studentGroup = await StudentGroupModel.requestStudentGroup(email, description, id);
+    const studentGroup = await StudentGroupModel.requestStudentGroup(email, description, id, group_name);
     if (studentGroup) {
         res.send(studentGroup.Item);
+    } else {
+        res.send('Failed to send request student group');
+    }
+});
+
+router.get('/requests/:status', async (req, res) => {
+
+    const status = req.params.status;
+
+    const requests = await StudentGroupModel.viewRequests(status);
+    if (requests) {
+        res.send(requests);
+    } else {
+        res.send('Failed to get student group requests');
+    }
+});
+
+router.put('/approve', async (req, res) => {
+    const {email, group_id, group_name} = req.body;
+
+    const result = await StudentGroupModel.acceptRequest(email, group_id, group_name);
+
+    if (result) {
+        res.send("Successfuly approved the request");
+    } else {
+        res.send('Failed to send request student group');
+    }
+});
+
+router.put('/reject', async (req, res) => {
+    const {email, group_id} = req.body;
+
+    const result = await StudentGroupModel.rejectRequest(email, group_id, group_name);
+    if (result) {
+        res.send("Successfuly rejected the request");
     } else {
         res.send('Failed to send request student group');
     }
