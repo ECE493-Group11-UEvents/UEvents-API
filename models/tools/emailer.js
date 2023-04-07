224aa368752a273989485d6fee4568f8dacca42a
syjiao@ualberta.ca <syjiao@ualberta.ca>
2023-04-07 15:20:38 -0600

    remove aws ses

--- models/tools/emailer.js
+++ models/tools/emailer.js
@@ -1,4 +1,3 @@
-const AWS = require('aws-sdk');
 const dotenv = require('dotenv');
 const sgMail = require('@sendgrid/mail');
 
@@ -8,52 +7,6 @@ const EMAIL_ADDRESS = process.env.EMAIL_ADDRESS;
 const EDIT_TEMPLATE_ID = process.env.SENDGRID_EDIT_TEMPLATE_ID;
 
 sgMail.setApiKey(process.env.SENDGRID_API_KEY);
-
-// AWS.config.update({
-//     region: process.env.REGION,
-//     accessKeyId: process.env.DB_ACCESS_KEY,
-//     secretAccessKey: process.env.DB_SECRET_ACCESS_KEY,
-// });
-
-// const ses = new AWS.SES();
-
-// class Emailer {
-//     /**
-//      * 
-//      * @param {[string]} email 
-//      * @param {string} subject 
-//      * @param {string} body 
-//      */
-//     static async sendEmail(email = [], subject, body){
-//         const params = {
-//             Destination: {
-//               ToAddresses: email
-//             },
-//             Message: {
-//                 Body: {
-//                     Text: {
-//                         Charset: 'UTF-8',
-//                         Data: body
-//                     },
-//                 },
-//                 Subject: {
-//                     Charset: 'UTF-8',
-//                     Data: subject,
-//                 },
-//             },
-//             Source: EMAIL_ADDRESS,
-//         };
-
-//         try {
-//             let res = await ses.sendEmail(params).promise();
-//             return res;
-//         }
-//         catch(err){
-//             console.log(err);
-//             return null;
-//         }
-//     }
-// }
 
 class Emailer {
     /**
