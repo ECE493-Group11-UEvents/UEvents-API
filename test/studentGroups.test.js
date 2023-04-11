c21904946726beb2f52df45edbc47b7631c88b85
syjiao@ualberta.ca <syjiao@ualberta.ca>
2023-04-10 19:44:00 -0600

    moved signup-login test and student group test into own test files

---
+++ test/studentGroups.test.js
@@ -0,0 +1,70 @@
+const chai = require('chai');
+const chaiHttp = require('chai-http');
+const app = require('../app');
+const expect = chai.expect;
+const AWS = require('aws-sdk');
+
+AWS.config.update({
+  region: process.env.REGION,
+  accessKeyId: process.env.DB_ACCESS_KEY,
+  secretAccessKey: process.env.DB_SECRET_ACCESS_KEY,
+});
+
+chai.use(chaiHttp);
+
+describe('Student Group Endpoints', () => {
+    let test_group = {
+        "group_id": "100000",
+        "group_name": "test group",
+        "description": "test description",
+        "photo_url": "test photo url"
+    };
+
+    it('Should return 200 and edit a new student group', (done) => {
+        chai.request(app)
+            .post(`/api/studentGroups/edit/${test_group.group_id}`)
+            .send(test_group)
+            .end((err, res) => {
+                expect(res).to.have.status(200);
+                done();
+            });
+    });
+
+    it('Should return 200 and get a student group', (done) => {
+        chai.request(app)
+            .get(`/api/studentGroups/${test_group.group_id}`)
+            .end((err, res) => {
+                expect(res).to.have.status(200);
+                expect(res.body).to.have.property('group_id');
+                expect(res.body).to.have.property('group_name');
+                expect(res.body).to.have.property('description');
+                expect(res.body).to.have.property('group_photo');
+                done();
+            });
+    });
+
+    it('Should return 200 and get a student group name', (done) => {
+        chai.request(app)
+            .get(`/api/studentGroups/${test_group.group_id}/name`)
+            .end((err, res) => {
+                console.log(res.body)
+                expect(res).to.have.status(200);
+                expect(res.body).to.have.property('S');
+                done();
+            });
+    });
+
+    it('Should return 200 and get student groups', (done) => {
+        chai.request(app)
+            .get('/api/studentGroups')
+            .end((err, res) => {
+                expect(res).to.have.status(200);
+                expect(res.body.Items).to.be.an('array');
+                expect(res.body.Items[0]).to.have.property('group_id');
+                expect(res.body.Items[0]).to.have.property('group_name');
+                expect(res.body.Items[0]).to.have.property('description');
+                expect(res.body.Items[0]).to.have.property('group_photo');
+                done();
+            });
+    });
+});
